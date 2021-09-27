4f2d8c204fa8ec915965c2b35d9afcadc37d1142
LiHuang <deadandlive@foxmail.com>
2021-09-27 14:17:22 +0800

    ss,ssr,vmess

---
+++ ssr.py
@@ -0,0 +1,12 @@
+import base64
+from bs4 import BeautifulSoup
+
+import requests
+
+if __name__ == '__main__':
+    url = "https://github.com/learnhard-cn/free_proxy_ss/blob/main/ssr/ssrsub"
+    content = requests.get(url).content
+    document = BeautifulSoup(content, features="lxml")
+    source = document.find("td", id="LC1").text
+    result = base64.b64decode(source).decode("utf-8")
+    print(result)
